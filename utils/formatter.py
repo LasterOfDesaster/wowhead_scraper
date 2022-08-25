65cd603c2875f43fd7ec3f01b8e6993c92e58f43
LasterOfDesaster <fabian.pfannerstill@gmx.de>
2022-08-25 11:00:53 +0200

    added comments on formatter.py

--- utils/formatter.py
+++ utils/formatter.py
@@ -100,6 +100,20 @@ class Formatter:
             g.write("\n")
     
     def __format_cons(self):
+
+        # read cons_data.json and load the json. create a export csv file for each type of consumes (0-8). changing the hard coded file name has to be done
+        # manually. this is just for test purposes and not intended to work in production
+        # Steps are
+        # 1.) generate a list of consumes ids (like in \ids\cons_ids.py) from the item.csv (wow.tools) filtered for SubClassID and put them into the cons ids file
+        # e.g. '117' : { 'classId' : 0, 'subClassId' : 5, 'material' : 0, },
+        # 2.) call 'python runner.py -l de -t cons' to execute ConsumesSpider and generate the json file (comment out the call to formatter in the spider_closed function)
+        # 3.) toggle comments line 6-12 in this file and call it manually to get the output csv file
+        # 4.) change 
+        # - the filename for the out file in __format_cons to output_cons_X_DESC.csv (X = SubClassID, DESC = short description)
+        # - the input file in __format_cons2 (same)
+        # - f("de", "cons") to f("de", "cons2"), 
+        # then call this file again to get the final output file
+
         with Path(self.lang_dir / "cons_data.json").open("r", encoding='utf-8') as f:
             cons_input = json.load(f)
         with Path(self.lang_dir / "output_cons_5_food+drinks.csv").open("w", encoding='utf-8') as g:
@@ -111,6 +125,8 @@ class Formatter:
             g.write("\n")
 
     def __format_cons2(self):
+
+        # since the json is different for each consume the following only applies to SubClassID = 5
         with Path(self.lang_dir / "output_cons_5_food+drinks.csv").open("r", encoding='utf-8') as f:
             fileName = self.lang_dir / "output_cons_5_food+drinks2.csv"
             if fileName.exists():
@@ -119,13 +135,21 @@ class Formatter:
             f.readline()
             lines = f.readlines()
 
+        # empty list of possible attributes consumes can have
         keys = []
+        # some items don't have a 'name_dede' attribute in their json representation. to fix this add this prop manually
         keys.append("name_enus")
+        # dict for the json_objects
         json_objects = {}
         for line in lines:              
             if line != "\n":
+                # split the line by semicolon: '{id};{title};{text};\n',
                 itemid = line.split(";")[0]
                 line = line.split(";")[-2]
+                # this part is the most tedious, since json requires string literals to have double quotes instead of single and the
+                # export only has single quoted strings, those have to be replaced. but unluckily some items contain single quotes in their
+                # name which would interfer with ending a string with double quotes. so after replacing ' with " the following part takes
+                # care of reversing this change for all the special cases like Tel"Abim -> Tel'Abim to maintain string delimination
                 line = line.replace("'", '"')
                 line = line.replace("Tel\"Abim", "Tel'Abim")
                 line = line.replace("\"Grimmigen Säufer\"", "'Grimmigen Säufer'")
@@ -138,6 +162,9 @@ class Formatter:
                 line = line.replace("Glyphe \"Naturkraft\" [PH]", "Glyphe 'Naturkraft' [PH]") 
                 line = line.replace("Star\"s Lament", "Star's Lament")                              
 
+                # each line has a json associated, convert this into json object and store it in the json_objects dict with item id as key
+                # then loop over it and store the prop name in the list of keys (if not already existing)
+                # every json has a prop called 'jsonequip which is a js object itself -> sub values have to be added too
                 cons_input = json.loads(line)
                 json_objects[itemid] = cons_input
                 for k,v in cons_input.items():
@@ -148,7 +175,15 @@ class Formatter:
                             if k2 not in keys:
                                 keys.append(k2)       
 
-
+        # all the lines are now stored as js objects in json_objects
+        # every possible prop is now listed in keys[]
+        # next step is to create the output csv file that consists of item id, name and a colum for each prop + associated value:
+        # 0.) write head line with semicolon separated values (all the possible keys) and setting it to empty-string
+        # 1.) loop over every entry in json_objects
+        # 2.) create temp dict and insert all keys[] 
+        # 3.) loop over the current entry's attributes and insert the value to the matching item in the output js object
+        # 3.1) if the current entry's attribute is jsonequip then just add the while stringifyed content and iterate over the subelements
+        # 4.) generate a output string starting with itemid and append delimiter ';' and the values from jo_out
         with Path(fileName).open("w", encoding='utf-8') as g:  
             output = "id;"
             for key in keys:
@@ -157,9 +192,7 @@ class Formatter:
             g.write(output + "\n")
 
             for jo in json_objects.items():
-                itemid = jo[0]
-                if itemid == '1401':
-                    itemid = itemid                   
+                itemid = jo[0]             
                 jo_out = {}
                 for key in keys:
                     jo_out[key] = ""
@@ -169,16 +202,12 @@ class Formatter:
                     if k == "jsonequip":
                         for k2, v2 in v.items():
                             jo_out[k2] = v2
-
              
                 output = itemid + ";"
                 for k,v in jo_out.items():
                     output = output + str(v) + ";"
 
                 g.write(output + "\n")                
-
-
-
 
             g.write("\n")        
 
